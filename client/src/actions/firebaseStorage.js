a38c064e1692bbbe7e7b15e8d9d5a2d56608594c
sotech117 <michael_foiani@brown.edu>
2021-12-02 21:00:12 -0500

    Add firebase storage uploading.

---
+++ client/src/actions/firebaseStorage.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "@firebase/app";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "@firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjRD3joAiTqKmzik78XAqwblshtZ5-fts",
+  authDomain: "triplemisw.firebaseapp.com",
+  projectId: "triplemisw",
+  storageBucket: "triplemisw.appspot.com",
+  messagingSenderId: "801413721622",
+  appId: "1:801413721622:web:a01b7ddabf3ee8ead2cf37"
+};
+const firebaseApp = initializeApp(firebaseConfig);
+
+const storage = getStorage(firebaseApp);
+
+/**
+ * Uploads any file inputted from an html input
+ * @param file the file must be an individual file, not the whole array
+ * @param setProgress the hook to update the progress state variable
+ * @param userId the user id from which the file comes
+ * @returns the url of the upload or NULL on failure
+ */
+export const uploadFile = (file, setProgress, userId) => {
+  return new Promise((resolve, reject) => {
+     // make storage ref based on user
+    const storageRef = ref(storage, `uploaded/${userId}/${file.name}`);
+
+    // start the upload
+    const task = uploadBytesResumable(storageRef, file)
+
+    // track progress
+    task.on('state_changed', snapshot => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      // set progress state from component
+      setProgress(progress);
+    }, error => {
+      console.error('Error uploading file in firebaseStorage.ts');
+      // null on fail
+      setProgress(0);
+      reject(error);
+    }, async () => {
+      // get the download url :)
+      const downloadUrl = await getDownloadURL(task.snapshot.ref);
+      setProgress(0);
+      resolve(downloadUrl);
+    })
+  })
+}
---
+++ client/src/components/UploadFile.js
@@ -0,0 +1,41 @@
+import React, { useRef, useState } from 'react'
+import { uploadFile } from '../actions/firebaseStorage'
+//import './.scss'
+
+// interface UploadFileProps {
+//   text: string
+//   editing: boolean
+//   onEdit: (newText: string) => void
+//   setEditing: (editing: boolean) => void
+// }
+// const { editing, text, onEdit, setEditing } = props
+
+export const UploadFile = () => {
+    const fileInputRef = useRef()
+    const [progress, setProgress] = useState(0)
+    
+    const uploadFiles = async () => {
+        const urls = []
+
+        const files = fileInputRef.current.files
+        for (let i = 0; i < files.length; i++) {
+            const file = files.item(i);
+            try {
+                const url = await uploadFile(file, setProgress, 'test');
+                urls.push(url);
+            } catch (error) {
+                console.error(`Failure to upload file ${file.name}`);
+            }
+        }
+        
+        // reset file input
+        fileInputRef.current.value = null;
+    }
+
+    return (
+    <div>
+            <input type="file" onChange={() => uploadFiles()} ref={fileInputRef} multiple></input>
+            <p>{progress}</p>
+    </div>
+    );
+}
